8a3125a295166dc11dfa9e71b90c5bdd07af15b8
krisliu <kris@nju>
2025-04-24 07:11:14 +0800

    script

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,8 @@
-CUDA_VISIBLE_DEVICES=0,1,2 python test_latency.py -n 2 -m /data/ubuntu/kris/checkpoints/Qwen__Qwen2.5-72B-Instruct-AWQ
+GPU_NUM=$1
+MODEL_PATH=$2
+CUDA_VISIBLE_DEVICES=0,1,6,7 python test_latency.py -n $GPU_NUM -m $MODEL_PATH
 
-CUDA_VISIBLE_DEVICES=0,1,2 python test_latency.py -n 2 -m /data/ubuntu/kris/checkpoints/Qwen__Qwen2.5-72B-Instruct
+# CUDA_VISIBLE_DEVICES=0,1,6,7 python test_latency.py -n $GPU_NUM -m /data/ubuntu/kris/checkpoints/Qwen__Qwen2.5-72B-Instruct
 
 # CUDA_VISIBLE_DEVICES=1,2 python test_latency.py -n 2 -m /data/ubuntu/kris/checkpoints/Qwen__Qwen2.5-72B-Instruct-AWQ
 
